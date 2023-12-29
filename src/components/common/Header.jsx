dd074fad37aa2af926f8e82d8c96b9f9f6cdedfb
aniket-infoware <anikets@infoware.ws>
2023-12-29 09:17:57 +0530

    updates

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <nav className="navbar navbar-light bg-light header p-0">
-    <div className="container-fluid"> 
+    <nav className="navbar navbar-light bg-light header p-4">
+    <div className="container-fluid p-0"> 
     <a className="navbar-brand logo" href="/">
        <div className='logo' style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}> 
        <img src="/images/logo.png" alt="" width="32" height="24" className="d-inline-block align-text-top" style={{marginRight: '5px'}}/>
